ffa20a8f22d43dbeae0e64baa9ed056e61fbb2ff
Alberth Mayta Cuevas <alberth.mayta@tecsup.edu.pe>
2025-11-20 12:47:53 -0500

    Update project files

--- Lab13AlberthMCuevas.Domain/obj/Debug/net9.0/Lab13AlberthMCuevas.Domain.AssemblyInfo.cs
+++ Lab13AlberthMCuevas.Domain/obj/Debug/net9.0/Lab13AlberthMCuevas.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab13AlberthMCuevas.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33faafc221c1045ce9f028113f3c5600197a8956")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab13AlberthMCuevas.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab13AlberthMCuevas.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
